6770b28c1dd354ef08ad2fd024b1f25f8f425be2
progg1992 <roggpierce@gmail.com>
2020-12-06 05:19:23 -0600

    Added validation and fixed security issues.

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const { json } = require('body-parser');
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '09b0aacf2dac493589d58a5e775a67a1'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db)=> {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db)=> {
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 }
