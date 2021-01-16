6713aa6369b09d65b4e2478badea9b9b8f337861
progg1992 <roggpierce@gmail.com>
2021-01-16 12:53:04 -0600

    updated dev port number

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image');
 const knex = require('knex');
 const { response } = require('express');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 const db = knex({
     client: 'pg',
